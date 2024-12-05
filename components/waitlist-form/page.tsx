dea45d100a583662b468c894e813f9b920c86cf3
Ramit <ramitsingh12@gmail.com>
2024-12-05 12:57:46 +0530

    fix

--- components/waitlist-form/page.tsx
+++ components/waitlist-form/page.tsx
@@ -19,14 +19,17 @@ const WaitlistForm: React.FC<WaitlistFormProps> = ({
       <div className={cn("space-y-2 lg:col-span-1",orientation =='vertical'?'':'pb-8')}>
       <Label htmlFor="input-01" aria-required className='text-textdark-200'>Name</Label>
       <div className="relative">
-        <Input required id="input-01" className="peer bg-surface border-textdark-300 py-1" placeholder="Enter your name here" type="text" />
+        <Input required id="input-01" className="peer ps-9 bg-surface border-textdark-300 py-1" placeholder="Enter your name here" type="text" />
+        <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/80 peer-disabled:opacity-50">
+        <Icon icon="material-symbols:person" className="text-textdark-200 text-xl" />
+        </div>
       </div>
     </div>
 
     <div className={cn("space-y-2 lg:col-span-1",orientation =='vertical'?'':'pb-8')}>
       <Label htmlFor="input-02" aria-required className='text-textdark-200'>Email</Label>
       <div className="relative">
-        <Input required id="input-02" className="peer py-1 ps-9" placeholder="Enter your email" type="email" />
+        <Input required id="input-02" className="peer py-1 ps-9 bg-surface border-textdark-300" placeholder="Enter your email" type="email" />
         <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/80 peer-disabled:opacity-50">
         <Icon icon="majesticons:mail-line" className="text-textdark-200 text-xl" />
         </div>
@@ -36,7 +39,7 @@ const WaitlistForm: React.FC<WaitlistFormProps> = ({
     <div className={cn("space-y-2 lg:col-span-1",orientation =='vertical'?'':'pb-8')}>
       <Label htmlFor="input-03" aria-required  className='text-textdark-200'>Phone</Label>
       <div className="relative">
-        <Input required id="input-03" className="peer py-1 ps-9" placeholder="Enter your phone no." type="number" />
+        <Input required id="input-03" className="peer py-1 ps-9 bg-surface border-textdark-300" placeholder="Enter your phone no." type="number" />
         <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/80 peer-disabled:opacity-50">
         <Icon icon="si:phone-fill" className="text-textdark-200 text-xl" />
         </div>
