80029d41fb2e14133060cdd48e2b5c84309ef5cc
Ramit <ramitsingh12@gmail.com>
2024-12-09 16:05:54 +0530

    removed video

--- components/sections/hero/page.tsx
+++ components/sections/hero/page.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <p className='text-white text-center text-pretty lg:text-lg lg:line-clamp-2 lg:max-w-2xl z-10'>Sanctorum is a coworking space in Gangtok designed for entrepreneurs, freelancers, and professionals who value a productive and collaborative environment.</p>
     <WaitlistForm orientation='horizontal'/>
     </div>
-        <div className="absolute inset-0 w-screen h-full z-0">    
+        {/* <div className="absolute inset-0 w-screen h-full z-0">    
         <div className="absolute inset-0 bg-black/40 z-10" />  
         <video  className="w-screen h-full scale-[200%]"
         autoPlay
@@ -20,7 +20,7 @@ const Hero = () => {
         Your browser does not support the video tag.
     
         </video>
-        </div>
+        </div> */}
        
    </section>
   )
