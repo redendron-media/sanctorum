04e061daea50a1ec1c43f772e7b2b863c82c629f
Ramit <ramitsingh12@gmail.com>
2024-12-09 16:13:44 +0530

    lint fix

--- components/sections/hero/page.tsx
+++ components/sections/hero/page.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
    <section className='flex relative flex-col h-full items-center w-full gap-4 lg:gap-8 bg-primary/50 overflow-hidden'>
     <div className='lg:px-10 px-10 py-[72px] lg:py-[180px] z-10 flex flex-col gap-4 lg:gap-8 items-center'>
-    <h1 className='mobile3 text-white font-im_fell text-balance text-center z-10'>It’s more than just a desk</h1>
+    <h1 className='mobile3 text-white font-im_fell text-balance text-center z-10'>It&apos;s more than just a desk</h1>
     <p className='text-white text-center text-pretty lg:text-lg lg:line-clamp-2 lg:max-w-2xl z-10'>Sanctorum is a coworking space in Gangtok designed for entrepreneurs, freelancers, and professionals who value a productive and collaborative environment.</p>
     <WaitlistForm orientation='horizontal'/>
     </div>
