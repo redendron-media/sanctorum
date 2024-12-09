a04fd0e369694252b3bd7c11a5db1553dd5782ed
Ramit <ramitsingh12@gmail.com>
2024-12-09 16:39:52 +0530

    line clamp fix

--- components/sections/events/page.tsx
+++ components/sections/events/page.tsx
@@ -6,7 +6,7 @@ const Events = () => {
     <section className="bg-surface gap-4 px-10 py-[72px] lg:py-[120px] lg:px-40 lg:gap-8 flex flex-col justify-center">
       <h1 className=" text-textdark text-center mobile3">Events</h1>
       <div className="flex flex-col gap-7 items-center">
-        <p className="text-textdark-300 text-center text-balance line-clamp-2 lg:text-lg lg:line-clamp-2 lg:max-w-2xl">
+        <p className="text-textdark-300 text-center text-balance lg:text-lg lg:line-clamp-2 lg:max-w-2xl">
           Get in the loop with talks, workshops, and creative sessions right
           here in Gangtok. From solo working sessions to community events, we
           bring together minds that inspire.
