1198b845b26893efc4275251232c8768359b9dfa
Ramit <ramitsingh12@gmail.com>
2024-12-10 12:11:31 +0530

    autoplay for carousel

--- components/sections/amenities/page.tsx
+++ components/sections/amenities/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   Carousel,
   CarouselContent,
@@ -6,19 +7,28 @@ import {
 import React from "react";
 import { amenities } from "@/constants/page";
 import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay"
 const Amenities = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
   return (
     <section className="py-[72px] lg:py-[120px] bg-primary/50 overflow-hidden w-full flex flex-col gap-8 lg:gap-12">
       <h1 className="mobile3 text-white text-center">Amenities</h1>
-      <Carousel>
+      <Carousel
+        plugins={[plugin.current]} 
+       
+        opts={{
+        loop:true,
+      }}>
         <CarouselContent className="mx-10">
           {amenities.map((amenity, index) => (
             <CarouselItem key={index} className="pl-7">
-              <div className="flex flex-col bg-textdark p-4 rounded-lg h-full gap-7 w-[211px] lg:w-[250px]">
+              <div className="flex flex-col bg-textdark p-4 rounded-lg h-fit gap-7 w-[211px] lg:w-[250px]">
                 <div className="h-[206px] w-[179px] lg:w-[218px] relative object-cover">
                 <Image className="select-none" src={`/amenities/${amenity.image}.png`} fill alt={amenity.title}/>
                 </div>
-                <div className="flex flex-col gap-2 justify-between grow">                  
+                <div className="flex flex-col gap-2">                  
                   <h4 className="mobile4 text-white grow select-none">{amenity.title}</h4>
                   <p className="text-white font-im_fell_pika leading-[18px] select-none text-xs w-full text-balance lg:text-sm">
                   {amenity.desc}
