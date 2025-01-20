a4cde4a0fbc16b7d81e6088dd9bb131515880a7a
Ramit <ramitsingh12@gmail.com>
2025-01-20 17:07:15 +0530

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { IM_Fell_English_SC,IM_Fell_DW_Pica } from "next/font/google";
 import Header from "@/components/header/page";
 import FloatingButton from "@/components/floating-button/page";
 import Footer from "@/components/footer/page";
+import Script from "next/script";
 
 const im_fell = IM_Fell_English_SC({
   weight: [ "400"],
@@ -33,6 +34,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          crossOrigin="anonymous"
+        >
+          {`
+         !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '467021129621361');
+fbq('track', 'PageView');
+
+        `}
+        </Script>
+      </head>
       <body
         className={`${im_fell_pika.className} ${im_fell.variable} antialiased`}
       >
